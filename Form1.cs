182da24b9434ed307e134117da874053f0e35f45
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2020-11-15 14:38:51 +0900

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-//using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
-//using System.Threading;
-//using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace iwm_ClipToText
