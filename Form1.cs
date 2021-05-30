d58c568a26ac8f0c073db1055749109e73f0edec
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2021-05-30 14:02:51 +0900

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ namespace iwm_ClipToText
 {
 	public partial class Form1 : Form
 	{
-		private const string VERSION = "クリップボードからテキストファイル生成 iwm20210217";
+		private const string VERSION = "クリップボードからテキストファイル生成 iwm20210529";
 		private const string NL = "\r\n";
 
 		private readonly string[] GblASExt = { "txt", "html", "csv", "tsv" };
@@ -38,10 +38,7 @@ namespace iwm_ClipToText
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			StartPosition = FormStartPosition.Manual;
-			DesktopLocation = new Point(
-				(Screen.PrimaryScreen.Bounds.Width - Width) / 2,
-				30
-			);
+			Form1_StartPosition();
 
 			Text = VERSION;
 
@@ -66,6 +63,40 @@ namespace iwm_ClipToText
 			TbSaveFileName.Select(0, 0);
 
 			SubTbResultReload(true);
+		}
+
+		private void Form1_StartPosition()
+		{
+			int WorkingAreaW = Screen.PrimaryScreen.WorkingArea.Width;
+			int WorkingAreaH = Screen.PrimaryScreen.WorkingArea.Height;
+
+			int WorkingAreaX = Screen.PrimaryScreen.WorkingArea.X;
+			int WorkingAreaY = Screen.PrimaryScreen.WorkingArea.Y;
+
+			int MouseX = Cursor.Position.X;
+			int MouseY = Cursor.Position.Y;
+
+			// X = Width
+			if (WorkingAreaW < MouseX + Size.Width)
+			{
+				MouseX -= Size.Width;
+				if (MouseX < 0)
+				{
+					MouseX = WorkingAreaX + 10;
+				}
+			}
+
+			// Y = Height
+			if (WorkingAreaH < MouseY + Size.Height)
+			{
+				MouseY -= Size.Height;
+				if (MouseY < 0)
+				{
+					MouseY = WorkingAreaY + 10;
+				}
+			}
+
+			Location = new Point(MouseX, MouseY);
 		}
 
 		private void TbResult_TextChanged(object sender, EventArgs e)
